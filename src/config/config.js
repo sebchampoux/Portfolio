0d23610e6f73d14a20929822ff5b20ba9e375e27
Sébastien Champoux <sebs.champoux@gmail.com>
2018-10-06 22:37:51 -0400

    Re-ordered some CSS

--- src/config/config.js
+++ src/config/config.js
@@ -6,6 +6,6 @@ export const config = {
 	 * URL de l'API Rest
 	 * @type {string}
 	 */
-	ajax_url: 'http://sebastienchampoux.com/wp-json/wp/v2',     // DEV
+	ajax_url: 'https://sebastienchampoux.com/wp-json/wp/v2',    // DEV
 	// ajax_url: portfolio_localiz.rest_url,                    // PROD
 };
