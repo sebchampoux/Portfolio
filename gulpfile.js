ffbbd6cd1df7845ad3918459d57f62d42ede2e88
Sébastien Champoux <contact@sebastienchampoux.com>
2019-12-16 16:15:19 -0500

    Ajouté browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
-const { src, dest, watch } = require('gulp');
+const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass');
 const rename = require('gulp-rename');
 const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
 const webpack_stream = require('webpack-stream');
 const webpack_config = require('./webpack.config');
+const browsersync = require('browser-sync').create();
 
 sass.compiler = require('node-sass');
 
@@ -19,20 +20,33 @@ function compileSass() {
 		.pipe(autoprefixer())
 		.pipe(rename('styles.css'))
 		.pipe(sourcemaps.write('.'))
-		.pipe(dest('./static/css'));
-}
-
-function compileJS() {
-	return webpack_stream(webpack_config)
-		.pipe(dest('./static'));
+		.pipe(dest('./static/css'))
+		.pipe(browsersync.stream());
+	}
+	
+	function compileJS() {
+		return webpack_stream(webpack_config)
+		.pipe(dest('./static'))
+		.pipe(browsersync.stream());
 }
 
 function compileOnChange() {
 	watch('./src/scss/**/*.scss', compileSass);
 	watch('./src/js/**/*.js', compileJS);
+	watch('./**/*.php').on('change', browsersync.reload);
+	watch('./**/*.twig').on('change', browsersync.reload);
+}
+
+function startBrowserSync(cb) {
+	browsersync.init({
+		proxy: 'localhost/portfolio',
+		notify: false,
+	});
+	cb();
 }
 
 exports.default = compileSass;
 exports.watch = compileOnChange;
 exports.js = compileJS;
 exports.sass = compileSass;
+exports.dev = series(startBrowserSync, compileOnChange);
